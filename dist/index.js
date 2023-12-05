51c235a4882441c9246a67d7fa0b8158c04c2f07
Geert Verstijnen <81244861+GeertVerstijnen@users.noreply.github.com>
2023-12-05 13:36:07 +0100

    seperated index.html into index.js

--- dist/index.js
+++ dist/index.js
@@ -1,50 +1,31 @@
+// script.js
 document.addEventListener("DOMContentLoaded", function () {
-    const carouselElement = document.getElementById("carousel-example");
+    const carousel = document.getElementById("carousel");
+    const progressBar = document.getElementById("progressBar");
+    const images = document.querySelectorAll("#carousel img");
+    const interval = 5000;
+    const animationDuration = 5000; // Duur van de animatie in milliseconden
+    let currentIndex = 0;
   
-    const items = [
-      {
-        position: 0,
-        el: document.getElementById("carousel-item-1"),
-      },
-      {
-        position: 1,
-        el: document.getElementById("carousel-item-2"),
-      },
-      {
-        position: 2,
-        el: document.getElementById("carousel-item-3"),
-      },
-    ];
+    function showNextImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      updateCarousel();
+      resetProgressBar();
+    }
   
-    // options with default values
-    const options = {
-      defaultPosition: 0, // Stel de standaardpositie in op de eerste afbeelding
-      interval: 3000, // Interval van 3 seconden tussen elke dia
+    function updateCarousel() {
+      const transformValue = -currentIndex * 100 + "%";
+      carousel.style.transform = "translateX(" + transformValue + ")";
+    }
   
-      // callback functions
-      onNext: () => {
-        console.log("next slider item is shown");
-      },
-      onPrev: () => {
-        console.log("previous slider item is shown");
-      },
-      onChange: () => {
-        console.log("new slider item has been shown");
-      },
-    };
+    function resetProgressBar() {
+      progressBar.style.transition = "none";
+      progressBar.style.transform = "scaleX(1)"; // Reset naar volle breedte
+      void progressBar.offsetWidth; // Force reflow
+      progressBar.style.transition = `transform ${animationDuration}ms linear`;
+      progressBar.style.transform = "scaleX(0)"; // Schaal naar nul breedte (links naar rechts)
+    }
   
-    // instance options object
-    const instanceOptions = {
-      id: "carousel-example",
-      override: true,
-    };
-  
-    // Maak een nieuw Carousel-instantie aan met de bovenstaande opties
-    const myCarousel = new Flowbite.Carousel(options, instanceOptions);
-  
-    // Start de automatische rotatie van de carousel
-    setInterval(() => {
-      myCarousel.next();
-    }, options.interval);
+    setInterval(showNextImage, interval);
   });
   
