5e56aa803e427e91bc17e813fbfc324fa30f33ac
Geert Verstijnen <81244861+GeertVerstijnen@users.noreply.github.com>
2024-01-15 11:23:41 +0100

    update!!!

--- dist/index.js
+++ dist/index.js
@@ -64,18 +64,8 @@ const generateCards = async () => {
             <div class="card-description">${doc.data().description}</div>
           </div>
           <div class="flex justify-start pb-2 gap-2">
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M13 20V18C13 15.2386 10.7614 13 8 13C5.23858 13 3 15.2386 3 18V20H13ZM13 20H21V19C21 16.0545 18.7614 14 16 14C14.5867 14 13.3103 14.6255 12.4009 15.6311M11 7C11 8.65685 9.65685 10 8 10C6.34315 10 5 8.65685 5 7C5 5.34315 6.34315 4 8 4C9.65685 4 11 5.34315 11 7ZM18 9C18 10.1046 17.1046 11 16 11C14.8954 11 14 10.1046 14 9C14 7.89543 14.8954 7 16 7C17.1046 7 18 7.89543 18 9Z"
-                stroke="#ffffff"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+
             <div class="flex justify-normal gap-2">
-            <div class="font-light italic card-progress">${doc.data().progress} / ${doc.data().total} </div>
-            <div class="text-red-500">(${percentage.toFixed(0)}%)</div>
             </div>
             </div>
         </div>
@@ -94,34 +84,34 @@ const generateCards = async () => {
 generateCards();
 
 
-document.addEventListener("DOMContentLoaded", function () {
-  const carousel = document.getElementById("carousel");
-  const progressBar = document.getElementById("progressBar");
-  const images = document.querySelectorAll("#carousel img");
-  const interval = 5000;
-  let currentIndex = 0;
+// document.addEventListener("DOMContentLoaded", function () {
+//   const carousel = document.getElementById("carousel");
+//   const progressBar = document.getElementById("progressBar");
+//   const images = document.querySelectorAll("#carousel img");
+//   const interval = 5000;
+//   let currentIndex = 0;
 
-  function showNextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    updateCarousel();
-    resetProgressBar();
-  }
+//   function showNextImage() {
+//     currentIndex = (currentIndex + 1) % images.length;
+//     updateCarousel();
+//     resetProgressBar();
+//   }
 
-  function updateCarousel() {
-    const transformValue = -currentIndex * 100 + "%";
-    carousel.style.transform = "translateX(" + transformValue + ")";
-  }
+//   function updateCarousel() {
+//     const transformValue = -currentIndex * 100 + "%";
+//     carousel.style.transform = "translateX(" + transformValue + ")";
+//   }
 
-  function resetProgressBar() {
-    progressBar.style.transition = "none";
-    progressBar.style.transform = "scaleX(1)"; // Reset naar volle breedte
-    void progressBar.offsetWidth; // Force reflow
-    progressBar.style.transition = `transform ${interval}ms linear`;
-    progressBar.style.transform = "scaleX(0)"; // Schaal naar nul breedte (links naar rechts)
-  }
+//   function resetProgressBar() {
+//     progressBar.style.transition = "none";
+//     progressBar.style.transform = "scaleX(1)"; // Reset naar volle breedte
+//     void progressBar.offsetWidth; // Force reflow
+//     progressBar.style.transition = `transform ${interval}ms linear`;
+//     progressBar.style.transform = "scaleX(0)"; // Schaal naar nul breedte (links naar rechts)
+//   }
 
-  setInterval(showNextImage, interval);
-});
+//   setInterval(showNextImage, interval);
+// });
 
 let savedName = localStorage.getItem('savedName') || '';
 
